9e942b9f499af4c62586a3905b7fe2416ea3fbd2
Becca Ricks <baricks@gmail.com>
2017-01-31 11:19:14 -0500

    added markov chain

--- web-scraper/breitbart.py
+++ web-scraper/breitbart.py
@@ -1,12 +1,12 @@
 import bs4
 import urllib
 
-url = "http://www.breitbart.com/big-journalism/"
+url = "http://www.breitbart.com/"
 
 html = urllib.urlopen(url).read()
 
 # Make beautiful soup object
-soup = bs4.BeautifulSoup(html)
+soup = bs4.BeautifulSoup(html, "html.parser")
 
 # Grab the titles
 titles = soup.select('h2.title')
